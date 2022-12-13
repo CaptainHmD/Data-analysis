ddbd8fd6e2112e3cdcdea61fc80945a9789910fa
ThomasTheCoder7 <vegetaxxxsan@gmail.com>
2022-12-13 13:10:21 +0300

    code cleanup

--- Main.py
+++ Main.py
@@ -1,38 +1,29 @@
 import snscrape.modules.twitter as twitterScraper
 import threading
-import timeit
-
-Hashtags = ["الحج و العمرة","جبل الرحمة","الحرم","المسجد النبوي","المشاعر المقدسة"]
-
+Hashtags = ["الحج و العمرة","جبل الرحمة","الحرم","المسجد النبوي","المشاعر المقدسة","الحج","العمرة","الجمرات","منى"]
+threads = []
 
 
-start = timeit.default_timer()
 def scrape(Hashtag):
-     start = "since:2013-12-27"
+     #set starting date and end date
+     start = "since:2021-12-29"
      end   = "until:2022-1-1"
+     #set Hashtag and date
      scraper = twitterScraper.TwitterSearchScraper(f"{Hashtag} {start} {end}")
-     with open(f"{Hashtag}.txt","w",encoding="utf-8") as file:          
+     #create txt file with hashtag and set encoding to utf-8 to support Arabic
+     with open(f"{Hashtag}.txt","w",encoding="utf-8") as file:
+       #for each tweet we scrape          
        for i,tweet in enumerate(scraper.get_items()):
-          file.write(f"{tweet.content}")
+          #if we got 50 break out of loop
+          if i > 50:
+            break
+          file.write(f"{tweet.rawContent}")
 
-
-thread1 = threading.Thread(target=scrape,args=(Hashtags[0],))
-thread2 = threading.Thread(target=scrape,args=(Hashtags[1],))
-thread3 = threading.Thread(target=scrape,args=(Hashtags[2],))
-thread4 = threading.Thread(target=scrape,args=(Hashtags[3],))
-thread5 = threading.Thread(target=scrape,args=(Hashtags[4],))
-
-thread1.start()
-thread2.start()
-thread3.start()
-thread4.start()
-thread5.start()
-
-thread1.join()
-thread2.join()
-thread3.join()
-thread4.join()
-thread5.join()
-
-
-stop = timeit.default_timer()
+#initialise all threads
+for i,text in enumerate(Hashtags):
+  threads.append(threading.Thread(target=scrape,args=(text,)))
+  threads[i].start()
+  
+#Make all threads wait after they finish
+for i in threads:
+  i.join()
